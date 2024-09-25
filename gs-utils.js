52b734382b611fa20bfc56832bb38f2e6da67fe1
mr21 <thomastortorini@gmail.com>
2024-09-25 16:37:45 +0200

    gs-utils: GSUuniqueName, use modern functions

--- gs-utils.js
+++ gs-utils.js
@@ -183,9 +183,9 @@ function GSUuuid() {
 function GSUuniqueName( nameOri, arr ) {
 	const name = GSUtrim2( nameOri );
 
-	if ( arr.indexOf( name ) > -1 ) {
+	if ( arr.includes( name ) ) {
 		const name2 = /-\d+$/u.test( name )
-			? name.substr( 0, name.lastIndexOf( "-" ) ).trim()
+			? name.substring( 0, name.lastIndexOf( "-" ) ).trim()
 			: name;
 		const reg = new RegExp( `^${ name2 }-(\\d+)$`, "u" );
 		const nb = arr.reduce( ( nb, str ) => {
