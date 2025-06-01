541d1898eac71664d0decb87f62f3febff57f6a7
mr21 <thomastortorini@gmail.com>
2025-06-01 04:46:08 +0200

    GSUsetTimeout/Interval + GSUclearTimeout/Interval

--- gs-utils-func.js
+++ gs-utils-func.js
@@ -4,6 +4,22 @@ function GSUnoop() {}
 function GSUnoopFalse() { return false; }
 
 // .............................................................................
+function GSUsetTimeout( fn, sec ) {
+	const ms = sec * 1000 | 0;
+
+	return ms
+		? setTimeout( fn, ms )
+		: ( fn(), null );
+}
+
+function GSUsetInterval( fn, sec ) {
+	return setInterval( fn, sec * 1000 | 0 );
+}
+
+function GSUclearTimeout( id ) { clearTimeout( id ); }
+function GSUclearInterval( id ) { clearInterval( id ); }
+
+// .............................................................................
 function GSUdebounce( fn, ms ) {
 	let timeoutId;
 
