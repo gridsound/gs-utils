1138bb3f0b386e1b79d28c87d1b8236f575db63e
mr21 <thomastortorini@gmail.com>
2025-09-09 06:58:11 +0200

    GSUdomIsDblClick

--- gs-utils-dom.js
+++ gs-utils-dom.js
@@ -54,6 +54,29 @@ function GSUdomEmpty( el ) {
 }
 
 // .............................................................................
+const _GSUdomIsDblClick_delay = 300;
+let _GSUdomIsDblClick_when = 0;
+let _GSUdomIsDblClick_elem = null;
+
+function GSUdomIsDblClick( e ) {
+	if ( e.button === 0 ) {
+		const now = Date.now();
+		const old = _GSUdomIsDblClick_elem?.deref();
+
+		if ( old !== e.target ) {
+			_GSUdomIsDblClick_elem = new WeakRef( e.target );
+			_GSUdomIsDblClick_when = now;
+		} else {
+			const dbl = now - _GSUdomIsDblClick_when < _GSUdomIsDblClick_delay;
+
+			_GSUdomIsDblClick_when = dbl ? 0 : now;
+			return dbl;
+		}
+	}
+	return false;
+}
+
+// .............................................................................
 function GSUdomSetChildrenLength( el, n, tag, prop ) {
 	if ( el.children.length < n ) {
 		el.append( ...GSUnewArray( n - el.children.length, () => GSUcreateElement( tag, prop ) ) );
