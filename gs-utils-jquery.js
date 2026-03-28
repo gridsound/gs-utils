05c2b615c3aa7c70fd8fe226ad00e5c6b79901a3
mr21 <thomastortorini@gmail.com>
2026-03-28 10:51:10 +0100

    GSUjq: $onclick + $oninput + $onchange + $input + $change

--- gs-utils-jquery.js
+++ gs-utils-jquery.js
@@ -184,11 +184,18 @@ class $$ {
 	}
 
 	// .........................................................................
+	$onclick( fn ) { return this.$on( "click", fn ); }
+	$oninput( fn ) { return this.$on( "input", fn ); }
+	$onchange( fn ) { return this.$on( "change", fn ); }
+	$click() { return this.$trigger( "click" ); }
+	$input() { return this.$trigger( "input" ); }
+	$change() { return this.$trigger( "change" ); }
+
+	// .........................................................................
 	$tag() { return this.#a0?.tagName.toLowerCase(); }
 	$focus() { return this.#a0?.focus( { preventScroll: true } ); }
 	$play() { return this.$trigger( "play" ); }
 	$pause() { return this.$trigger( "pause" ); }
-	$click() { return this.$trigger( "click" ); }
 	$text( v ) { return this.$prop( "textContent", v ); }
 	$value( v ) { return this.$prop( "value", v ); }
 	$checked( b ) { return this.$prop( "checked", b ); }
